5d31f037cae46aa64ee64481bf8bbebf59460f64
YoonJunHyeon <qmdldos@gmail.com>
2023-12-11 15:50:15 +0900

    feat : express를 활용한 server.js 생성; 서버 초기화

---
+++ server/servers/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+// JSON 요청 본문 파싱
+
+// 추가 미들웨어 들어갈 자리
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT => {
+  console.log(`http://localhost:${PORT}`);
+})
+
