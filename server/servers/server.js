3642f4636ad420077508717f5f8acb4743e2e3f3
YoonJunHyeon <qmdldos@gmail.com>
2023-12-11 16:18:38 +0900

    너무 복잡하다 뒤엎어

--- server/servers/server.js
+++ server/servers/server.js
@@ -5,7 +5,9 @@ app.use(express.json());
 // JSON 요청 본문 파싱
 
 // 추가 미들웨어 들어갈 자리
-
+const userRoutes = require('./router/userRoutes')
+// 라우터 사용
+app.use('/routes/userRoutes', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT => {
