5792098aecec7a7ad7dd0ee35b7e4033865db4ce
YoonJunHyeon <qmdldos@gmail.com>
2023-12-27 18:05:51 +0900

    --중간 저장

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/app/components/Header/Header";
 import Nav from "@/app/components/Nav/Nav"; 
-import CartComponent from "@/app/components/Main/CartComponent";
+import CartComponent from "@/app/components/Main/CartComp/CartComponent";
 import Footer from "@/app/components/Footer/Footer";
 
 export default function Cart() {
