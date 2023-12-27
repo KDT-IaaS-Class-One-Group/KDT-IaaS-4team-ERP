d22ce177c380574f0b388ab87086778916f71f04
YoonJunHyeon <qmdldos@gmail.com>
2023-12-27 15:43:00 +0900

    done : 장바구니 작성완료

--- src/app/components/Main/CartComponent.tsx
+++ src/app/components/Main/CartComponent.tsx
@@ -1,14 +1,18 @@
 import React from "react"; 
 import CartNav from "../CartNav/CartNav";
 import Btn from "../Btn/Btn";
+import ProductList from "../productList/ProductList";
 
 
 export default function CartComponent() {
   return (
-    <main>
-      <Btn textContent='주문하기'/>
+    <main className="flex flex-col overflow-hidden">
+      <Btn textContent='주문하기' className="h-10 w-28 outline outline-1 flex items-center justify-center "/>
       <CartNav />
-      {/* 대충 제품 */}
+      <ul id="productUl">
+        {/*제품리스트가 추가 될 곳*/}
+        <ProductList pUrl=''pCount={1} pPrice={150000} pSub="이 상품은 예시입니다." />
+      </ul>
     </main>
   );
 }
---
+++ src/app/components/productList/ProductList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Btn from '../Btn/Btn';
+import Image from 'next/image';
+
+interface PList {
+  pUrl: string;
+  pSub: string;
+  pPrice: number;
+  pCount: number;
+}
+
+const ProductList: React.FC<PList> = ({pUrl, pSub, pPrice, pCount}) => {
+  return (
+    <li 
+    className='w-full flex justify-between items-center border-2 border-slate-800 p-4'>
+      <Image src={pUrl} alt="이미지가 들어갈 자리입니다." width="150" height = "150"/>
+      <p className='text-xs'>{pSub}</p>
+      <div >{pPrice}</div>
+      <div>{pCount}</div>
+      <Btn textContent='삭제'
+        className='h-10 w-28 outline outline-1 flex items-center justify-center '/>
+      <select name="option" id="pOption" 
+        className='border-1 border-slate-600'>
+        <option value="1">1개</option>
+        <option value="2">2개</option>
+        <option value="3">3개</option>
+      </select>
+    </li>
+  );
+}
+
+export default ProductList;
