a5cef88ed4669cee0277f8dd39717a1013327935
HongMoonGI <hmoongi94@gmail.com>
2023-12-27 16:46:39 +0900

    feat: css적용

--- src/app/components/Payment/buyerInformation.tsx
+++ src/app/components/Payment/buyerInformation.tsx
@@ -5,24 +5,26 @@ export default function buyerInformation() {
 
   return (
     <div className="flex justify-center items-center">
-      <div className={`${styles.buyerInformation} flex flex-col justify-center`}>
+      <div
+        className={`${styles.buyerInformation} flex flex-col justify-center`}
+      >
         {/* tailwind와 css모듈 같이 사용하기 */}
-        <div className="relative mt-30 mb-30">
+        <div className="relative mt-2 mb-2">
           <p>배송정보:</p>
           <p>product</p>
         </div>
 
-        <div>
+        <div className="relative mt-2 mb-2">
           <p>이름/연락처:</p>
           <p>buyerInfo</p>
         </div>
 
-        <div>
+        <div className="relative mt-2 mb-2">
           <p>주소:</p>
           <p>address</p>
         </div>
 
-        <div>
+        <div className="relative mt-2 mb-2">
           <p>요청사항:</p>
           <p>request</p>
         </div>
