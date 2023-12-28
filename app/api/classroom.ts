7998fa7f2eff13aa0ee1010940b272f39566b942
이민구 <escapeweedy@gmail.com>
2023-12-28 12:40:00 +0900

    feat: db조회 api

---
+++ app/api/classroom.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { ClassroomManager } from '../module/classroomManager';
+import data from '../databases/classroom.json'; // 데이터 파일 불러오기
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const manager = new ClassroomManager(data);
+
+  // 예시: 특정 강의실 조회
+  const room301 = manager.getClassroom('room_301');
+
+  res.status(200).json(room301);
+}
