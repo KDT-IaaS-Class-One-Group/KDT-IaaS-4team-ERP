73fe465e1ad84d69e0fd8e08eafbabb2925b4d4a
이민구 <escapeweedy@gmail.com>
2023-12-28 13:01:45 +0900

    feat: 단순 조회 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+import classroomData from './databases/classroom.json';
+
 export default function Home() {
-  return <main>test</main>;
+  return (
+    <div>
+      {Object.entries(classroomData).map(([roomKey, roomDetails]) => (
+        <div key={roomKey}>
+          <h2>{`교실: ${roomKey}`}</h2>
+          <p>{`강사: ${roomDetails.instructor}`}</p>
+          <p>{`분야: ${roomDetails.field}`}</p>
+          <p>{`컴퓨터 수: ${roomDetails.computers}`}</p>
+          <h3>학생 목록:</h3>
+          <ul>
+            {roomDetails.students.map((student) => (
+              <li key={student}>{student}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
