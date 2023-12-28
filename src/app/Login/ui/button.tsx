8f4ba2ab10afceacb2431292cfff70b7733be001
kimhyun <forhn37@gmail.com>
2023-12-28 14:35:03 +0900

    feat : tailwind, css모듈을 두가지 활용한 로그인페이지 완성

--- src/app/Login/ui/button.tsx
+++ src/app/Login/ui/button.tsx
@@ -3,6 +3,6 @@ import styles from './button.module.css'
 
 export default function LoginPageButton({buttonname}) {
   return (
-    <div className={styles.button}>{buttonname}</div>
+    <button className={styles.button}>{buttonname}</button>
   )
 }
