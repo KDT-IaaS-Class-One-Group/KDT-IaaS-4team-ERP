91ffe98fd432e0805b6de31cbe542ebb599d4643
HongMoonGI <hmoongi94@gmail.com>
2023-12-28 12:06:49 +0900

    test: css모듈이 tailwind보다 우선순위?

--- src/app/components/Payment/buyerInformation.tsx
+++ src/app/components/Payment/buyerInformation.tsx
@@ -6,8 +6,9 @@ export default function buyerInformation() {
   return (
     <div className="flex justify-center items-center mt-10">
       <div
-        className={`${styles.buyerInformation} bg-red-500 flex flex-col justify-center`}
+        className={`bg-red-500 flex ${styles.buyerInformation} flex-col justify-center`}
       >
+        {/* css모듈이 tailwind보다 우선순위? */}
         {/* tailwind와 css모듈 같이 사용하기 */}
         <div className="relative mt-2 mb-2">
           <p>배송정보:</p>
