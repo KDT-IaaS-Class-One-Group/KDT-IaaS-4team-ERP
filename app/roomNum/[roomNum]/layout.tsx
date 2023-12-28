c17d765bd8538059e6b5d44da538bca0b2425274
YoonJunHyeon <qmdldos@gmail.com>
2023-12-28 17:30:04 +0900

    docs : 주석에 슈도코드 작성완료

--- app/roomNum/[roomNum]/layout.tsx
+++ app/roomNum/[roomNum]/layout.tsx
@@ -2,7 +2,6 @@
 
 import { usePathname } from 'next/navigation';
 
-
 export default function RoomLayout ({
   children,
 }: {
@@ -12,12 +11,18 @@ export default function RoomLayout ({
   return (
     <div id='root' className='w-screen h-screen'>
       <div className='upper outline outline-1 w-full h-1/5 flex justify-center items-center flex-col'>
-        <h1>{extractedString}반에 오신 걸 환영합니다.</h1>
-        <h3>강사 : </h3>
-        <h3>과목 : </h3>
+        <h1 className='text-3xl text-slate-900 font-bold mb-4'>{extractedString}반에 오신 걸 환영합니다.</h1>
+        <h3>강사 : 
+          {/* 강사 이름이 들어갈 자리 */}
+        </h3>
+        <h3>과목 : 
+          {/* 과목이 들어갈 자리 */}
+        </h3>
       </div>
       <div className='lower w-full h-4/5 flex justify-center items-center gap-6'>
         {children}
+        {/* 이 자리에는 객체의 컴퓨터 갯수만큼 컴포넌트가 호출이 됩니다. */}
+        {/* 그 후 1번 컴퓨터 부터 학생의 이름이 들어갑니다. */}
       </div>
     </div>
   );
--- app/roomNum/[roomNum]/page.tsx
+++ app/roomNum/[roomNum]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function ProductDetails() {
-  return <div>
+  return <div className='outline outline-1 w-20 h-20'>
     
   </div>;
 }
