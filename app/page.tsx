f1a6cb4e6bb3247bdf2aafcd032f42d0bb36887b
이민구 <escapeweedy@gmail.com>
2023-12-28 17:57:06 +0900

    feat: 불필요 내용 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,7 @@ interface Props {
 export default function Home() {
   return (
     <div>
-      {Object.entries(classroomData).map(([room, details]) => (
-        <div key={room}>
-          <h1>{`Room ${room}`}</h1>
-          <p>{`Instructor: ${details.instructor}`}</p>
-          {/* 나머지 데이터도 이와 같이 UI에 표시할 수 있습니다. */}
-        </div>
-      ))}
+
     </div>
   );
 }
