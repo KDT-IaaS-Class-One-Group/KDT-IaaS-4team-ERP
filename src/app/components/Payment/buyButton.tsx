0295c81b1629f33cbb32c3a5a9637bd408664909
HongMoonGI <hmoongi94@gmail.com>
2024-01-02 12:53:17 +0900

    feat: 구매완료 버튼 UI제작

--- src/app/components/Payment/buyButton.tsx
+++ src/app/components/Payment/buyButton.tsx
@@ -1,9 +1,13 @@
-// import styles from "./style/buyerInformation.module.css";
+// import styles from "./style/buyButton.module.css";
 
 export default function buyButton() {
   // const buyerinformation = await fetchbuyerinformationdata()
 
   return (
-    <div></div>
+    <div className="flex justify-center relative mt-5">
+      <div className="flex flex-row-reverse w-4/5">
+        <button className="bg-slate-300">구매완료</button>
+      </div>
+    </div>
   );
 }
