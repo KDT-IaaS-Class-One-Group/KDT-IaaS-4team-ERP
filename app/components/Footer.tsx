7310428a2aa4bddd5000d4904cbf31f499d1cb27
이민구 <escapeweedy@gmail.com>
2024-01-02 13:00:11 +0900

    feat: 컴포넌트 레이아웃 제작

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <footer className='bg-gray-200 text-center p-4 mt-8'>
-      <p>ⓒ</p>
+      <p>ⓒ 2024</p>
     </footer>
   );
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,15 @@
 export default function Header() {
   return (
-    <header className='bg-blue-500 text-white text-center p-4'>
-      <h1 className='text-xl'>나의 웹 쇼핑몰</h1>
+    <header className='flex bg-blue-500 text-white p-4'>
+      <h1 className='text-xl'>form 미쳤다.</h1>
+      <nav>
+        <ul className='flex justify-between'>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul>
+      </nav>
     </header>
   );
 }
