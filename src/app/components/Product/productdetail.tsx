1fa679c7e83b86f3ad884e307842280d39ca940b
HongMoonGI <hmoongi94@gmail.com>
2024-01-02 18:10:52 +0900

    feat: 상품상세란 UI만들기(중간 커밋)

--- src/app/components/Product/productdetail.tsx
+++ src/app/components/Product/productdetail.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./style/productdetail.module.css";
 import { ProductDetail } from "./types";
 
 type ProductDetailProps = {
@@ -7,29 +8,34 @@ type ProductDetailProps = {
 
 const ProductDetail: React.FC<ProductDetailProps> = ({ productdetail }) => {
   return (
-    <div className="product-detail">
-      <div>이미지 자리</div>
-      {/* <img src={productdetail.Img} alt={productdetail.Img} /> */}
+    <div className="product-detail w-full h-screen flex flex-col items-center">
+      <div className="w-4/5 h-2/5 flex justify-around">
+        <div className="w-1/2 h-full bg-slate-300 flex items-center justify-center ">
+          이미지 자리
+        </div>
+        {/* <img src={productdetail.Img} alt={productdetail.Img} /> */}
 
-      <div>
-        <p>상품 제목: </p>
-        {/* {productdetail.Title} */}
-        <p>가격: </p>
-        {/* {productdetail.Price} */}
-        <p>상품 간략설명: </p>
-        {/* {productdetail.Summary} */}   
-        <div>수량 올라가는 기능넣는 컴포넌트 자리</div>
-        <button>구매</button>     
-        <button>장바구니</button>     
-        <button>상품평</button>     
+        <div className="mr-40">
+          <p>상품 제목: </p>
+          {/* {productdetail.Title} */}
+          <p>가격: </p>
+          {/* {productdetail.Price} */}
+          <p>상품 간략설명: </p>
+          {/* {productdetail.Summary} */}
+          <div>수량 올라가는 기능넣는 컴포넌트 자리</div>
+          <div className="flex justify-around">
+            {/* 링크로 바꿔야되나? */}
+            <button className="bg-red-300">구매</button>
+            <button className="bg-red-300">장바구니</button>
+            <button className="bg-red-300">상품평</button>
+          </div>
+        </div>
       </div>
 
-      <div>
-        상품 설명란
-        (오버되는 내용 스크롤)
+      <div className="w-4/5 flex justify-center items-center">
+        상품 설명란 (오버되는 내용 스크롤)
         {/* {productdetail.Detail} */}
       </div>
-
     </div>
   );
 };
