10385ac89d44a26293aadb77f6adcfcf9e989946
이민구 <escapeweedy@gmail.com>
2024-01-02 11:30:05 +0900

    feat: kr을 ko로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='kr'>
+    <html lang='ko'>
       <body className={inter.className}>{children}</body>
     </html>
   );
