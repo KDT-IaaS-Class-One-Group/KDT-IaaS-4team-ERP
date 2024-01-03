c65d56cdc4da4ab560cf74990d7ea342e0a51c05
kimhyun <forhn37@gmail.com>
2024-01-03 16:19:59 +0900

    feat : ProductCcomment 페이지 구조 변경

--- app/(customer)/ProductComment/page.tsx
+++ app/(customer)/ProductComment/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react';
-import ProductCommentList from './ui/ProductCommentList';
+import ProductCommentList from '../../components/ProducComment/ProductCommentList';
 
 const CommentHome = () => {
   const [comments, setComments] = useState([]);
