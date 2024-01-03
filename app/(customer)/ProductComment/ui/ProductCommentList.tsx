833b30893cf0968d25a41159f256d301d2d1c5d3
kimhyun <forhn37@gmail.com>
2024-01-03 12:57:55 +0900

    feat : 상품평 페이지 작성 완료 - merge예정

--- app/(customer)/ProductComment/ui/ProductCommentList.tsx
+++ app/(customer)/ProductComment/ui/ProductCommentList.tsx
@@ -1,10 +1,10 @@
 const ProductCommentList = ({writerid, date, starcount, commenttitle}) => {
   return (
-    <div>
-        <div>{writerid}</div>
-        <div>{date}</div>
-        <div>{starcount}</div>
-        <div>{commenttitle}</div>
+    <div className="flex justify-around items-center w-5/6 h-28 outline">
+        <div className="outline w-16 h-8 flex justify-around items-center">{writerid}</div>
+        <div className="outline w-32 h-8 flex justify-around items-center">{date}</div>
+        <div className="outline w-32 h-8 flex justify-around items-center">{starcount}</div>
+        <div className="outline w-56 h-8 flex justify-around items-center">{commenttitle}</div>
     </div>
   )
 }
