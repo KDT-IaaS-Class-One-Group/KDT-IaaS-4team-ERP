a899023dea2d429a4dbcb8ded6607efd4c3e82b1
kimhyun <forhn37@gmail.com>
2024-01-07 15:45:49 +0900

    feat : login 작동 완료

--- app/customComp/Login/loginbutton.tsx
+++ app/customComp/Login/loginbutton.tsx
@@ -1,6 +1,6 @@
-const Loginbutton = ({value}) => {
+const Loginbutton = ({value, onClick}) => {
   return (
-    <button type="button" className="w-36 h-16 border-solid border-2 mt-2 ml-2" >{value}</button> 
+    <button type="button" className="w-36 h-16 border-solid border-2 mt-2 ml-2" onClick={onClick}>{value}</button> 
   )
 }
 
--- app/customComp/Login/logintext.tsx
+++ app/customComp/Login/logintext.tsx
@@ -1,9 +1,12 @@
-const Logintext = ({title}) => {
+'use client'
+const Logintext = ({title, inputchange}) => {
 
   return (
     <div className="flex h-20 m-2 w-full justify-between">
       <label htmlFor="Idiputtext" className="w-32 text-center flex justify-center items-center ">{`${title} :`}</label>
-      <input type="text" id="Idiputtext" className="w-3/5 border-solid border-2" />
+      <input type="text" id="Idiputtext" className="w-3/5 border-solid border-2" onChange={(e)=> {
+        inputchange(e.target.value)
+      }}/>
     </div>
   )
 }
