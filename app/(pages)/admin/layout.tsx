e11885200ebb274542f09705154eec70455b31fd
이민구 <escapeweedy@gmail.com>
2024-01-08 12:09:55 +0900

    feat: 오타 수정

--- app/(pages)/admin/layout.tsx
+++ app/(pages)/admin/layout.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Header from '@components/Header/Header';
-import Footer from '@components/Footer/Footer';
-import ChildrenProps from '@interfaces/ChildrenProps';
-import HeaderItem from '@components/Header/HeaderItem';
-import Sidebar from '@components/Sidebar/Sidebar';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import ChildrenProps from '@/interfaces/ChildrenProps';
+import HeaderItem from '@/components/Header/HeaderItem';
+import Sidebar from '@/components/Sidebar/Sidebar';
 
 export default function CustomerLayout({ children }: ChildrenProps) {
   return (
