7189be0475498dfb9d69f9226e492100203857a6
kimhyun <forhn37@gmail.com>
2024-01-08 12:32:26 +0900

    feat : 테스트중

---
+++ app/(customer)/[id]/product/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import ProductDetail from "@/app/customComp/Product/productdetail";
+
+export default function ProductPage() {
+
+  return (
+    <>
+      <ProductDetail />
+    </>
+  );
+};
+
+
--- app/customComp/Login/logincomponent.tsx
+++ app/customComp/Login/logincomponent.tsx
@@ -24,7 +24,7 @@ const LoginHome = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch(`http://localhost:3560/login`, {
+      const response = await fetch(`192.168.100.76/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
