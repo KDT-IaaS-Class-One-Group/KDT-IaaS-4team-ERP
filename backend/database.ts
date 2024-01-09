554d2a28e1ccb82f2907afbec8e0e0636785bc08
Pingu Lee <escapeweedy@gmail.com>
2024-01-09 15:50:23 +0900

    feat: database pool 분리

---
+++ backend/database.ts
@@ -0,0 +1,13 @@
+import mariadb from 'mariadb';
+
+// MariaDB 연결 풀 설정
+const pool = mariadb.createPool({
+  host: '192.168.100.83',
+  port: 3309,
+  user: 'root',
+  password: 'root',
+  database: 'form',
+  connectionLimit: 5,
+});
+
+export default pool;
