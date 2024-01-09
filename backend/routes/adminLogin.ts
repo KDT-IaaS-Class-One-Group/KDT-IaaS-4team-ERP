c8769ae24823ad659320ae4db426a43f569899a8
Pingu Lee <escapeweedy@gmail.com>
2024-01-09 16:38:11 +0900

    feat: 불필요 파일 삭제

--- backend/routes/adminLogin.ts
+++ backend/routes/adminLogin.ts
@@ -1,23 +1,29 @@
-import { Router } from 'express';
-import mariadb from 'mariadb';
+import express from 'express';
 import pool from '../database';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
-const adminLogin = Router();
+const adminLogin = express();
+const port = 3001;
+
+adminLogin.use(cors());
+adminLogin.use(bodyParser.json());
 
 adminLogin.post('/admin/login', async (req, res) => {
+  console.log('test');
   let conn;
   try {
     conn = await pool.getConnection();
-    const { username, password } = req.body;
+    const { userid, password } = req.body;
     const rows = await conn.query(
-      'SELECT * FROM administrators WHERE username = ? AND password = ?',
-      [username, password],
+      'SELECT * FROM administrators WHERE userid = ? AND password = ?',
+      [userid, password],
     );
 
     if (rows.length > 0) {
-      res.json({ success: true, message: '로그인 성공' });
+      res.json({ success: true });
     } else {
-      res.json({ success: false, message: '잘못된 사용자 이름 또는 비밀번호' });
+      res.json({ success: false });
     }
   } catch (err) {
     res.status(500).send('로그인 처리 중 오류 발생: ' + err);
--- backend/server.ts
+++ backend/server.ts
@@ -9,8 +9,7 @@ const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-// 관리자 로그인
-app.use('/admin/login', adminLogin);
+app.post('/admin/login', adminLogin);
 
 app.listen(port, () => {
   console.log(`Express 서버가 ${port}번 포트에서 실행중입니다.`);
