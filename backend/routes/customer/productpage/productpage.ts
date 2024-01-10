44ec4af87005d7ac9d9640312d228b04dae5599a
HongMoonGI <hmoongi94@gmail.com>
2024-01-10 18:05:59 +0900

    feat: 상품페이지 동적라우팅으로 데이터 보내게 수정

--- backend/routes/customer/productpage/productpage.ts
+++ backend/routes/customer/productpage/productpage.ts
@@ -6,18 +6,30 @@ import pool from "../../../database";
 
 const product = express();
 
-product.get("/product", async (req, res) => {
+// :prodIndex 값으로 동적 라우팅하면 그에 맞는 데이터를 보내줌.
+product.get("/product/:prodIndex", async (req, res) => {
   let conn;
+
+  const prodIndex = parseInt(req.params.prodIndex, 10);
   try {
     conn = await pool.getConnection();
-    const result = await conn.query("SELECT * FROM products");
-    res.status(200).json(result);
+
+    // MySQL 쿼리 실행
+    const [result] = await conn.query('SELECT * FROM products WHERE prodIndex = ?', [prodIndex]);
+
+    if (result.length === 0) {
+      // 상품이 없을 경우 404 에러 전송
+      res.status(404).json({ error: 'Product not found' });
+    } else {
+      // 결과를 클라이언트로 전송
+      res.json(result[0]); // 첫 번째 결과만 전송
+    }
   } catch (error) {
-    console.error("Error fetching products:", error);
-    res.status(500).json({ error: "Error fetching products" });
+    console.error("Error fetching product:", error);
+    res.status(500).json({ error: "Error fetching product" });
   } finally {
     if (conn) conn.release();
   }
 });
 
-export default product
+export default product;
