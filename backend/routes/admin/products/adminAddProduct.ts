b63d0463fb56994e194cc3e96ac560fdd2fdae13
Pingu Lee <escapeweedy@gmail.com>
2024-01-11 17:03:32 +0900

    bug: TypeError: Do not know how to serialize a BigInt     at JSON.stringify (<anonymous>)

--- backend/routes/admin/products/adminAddProduct.ts
+++ backend/routes/admin/products/adminAddProduct.ts
@@ -13,15 +13,7 @@ adminAddProduct.post('/api/addproduct', async (req, res) => {
       prodPrice,
       prodStock,
     } = req.body;
-    // 문자열로 받은 prodPrice와 prodStock를 숫자로 변환
-    const numericPrice = Number(prodPrice);
-    const numericStock = Number(prodStock);
-
-    if (isNaN(numericPrice) || isNaN(numericStock)) {
-      // 숫자로 변환할 수 없는 경우 에러 처리
-      res.status(400).send('Invalid numeric values for prodPrice or prodStock');
-      return;
-    }
+    console.log(prodStock, prodPrice);
 
     const conn = await pool.getConnection();
     const query =
@@ -32,9 +24,10 @@ adminAddProduct.post('/api/addproduct', async (req, res) => {
       prodDescription,
       prodCategory,
       prodImgUrl,
-      numericPrice, // 숫자로 변환된 값으로 사용
-      numericStock, // 숫자로 변환된 값으로 사용
+      Number(prodPrice),
+      Number(prodStock),
     ]);
+
     conn.release();
 
     res.status(201).send({ prodIndex: result.insertId });
