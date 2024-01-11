7f1b4210c336c609e139885e54d87507de5597dd
DongHyunShin <personarpg7501@gmail.com>
2024-01-11 14:32:06 +0900

    feat : 로컬 환경에서 DB의 데이터를 josn 변환 후 세팅

--- frontend/app/(pages)/(customer)/productcomment/page.tsx
+++ frontend/app/(pages)/(customer)/productcomment/page.tsx
@@ -15,21 +15,51 @@ interface productcommentfull {
   reviewTitle: string;
 }
 
-
 export default function Productcommentfull() {
-  const [productwhole, setProductComment] = useState([])
+  const [productcomment, setProductComment] = useState([]);
   useEffect(() => {
     const fetchProductcomment = async () => {
       try {
-        const response = await fetch('http://localhost:3560/productcomment');
-        const data = await response.json();
-        console.log(data)
+        const response = await fetch('http://localhost:3560/productcomment'); //로컬환경 테스트 실행 () http://192.168.100.83:3560/productcomment)
+        const data = await response.json();//DB에서 가져온 데이터를 json으로 변환
+        console.log(data) //콘솔에서 넘어온 데이터 확인
+        const commentData = data.map(list => ({
+          userId : list.userId,
+          reviewUpdatedAt : list.reviewCreateAt,
+          reviewRating : list.reviewRating,
+          reviewTitle : list.reviewTitle
+        }))
+        console.log(commentData);
       } catch (error) {
         console.error('데이터를 불러오는 동안 에러 발생:', error);
       }
     }
-  })
-  return;
+    // 함수를 호출하여 데이터 가져오기
+    fetchProductcomment(); //호출이 없으면DB 데이터가 안들어옴
+  },[])
+
+
+  return (
+    <>
+      <div className='w-screen h-screen flex justify-start items-center flex-col gap-6'>
+        {productcomment.map((list, index) => (
+          <ProductCommentList
+            key={index}
+            writerid={list.userId}
+            date={list.reviewContent}
+            starcount={list.reviewRating}
+            commenttitle={list.reviewTitle}
+          />
+        ))}
+      </div>
+      <Link href="/productcommentwriting">
+        <div className='mr-40 mb-20'>
+          <ProductUploadButton value='상품평 등록' />
+        </div>
+      </Link>
+    </>
+  );
+  ;
 }
 
 
