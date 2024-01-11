3786d9dbdb251eeb7fd0b3d8a0b2587cf1a97f48
kimhyun <forhn37@gmail.com>
2024-01-11 12:53:48 +0900

    feat : 준현아!!!

---
+++ frontend/app/(pages)/(customer)/product/[productdetail]/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import ProductDetail from "@/components/Product/productdetail";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+// import { useRouter } from "next/router";
+
+export default function ProductPage() {
+  const [productdata, setproductdata] =useState([])
+  const {productdetail} = useParams();
+
+  useEffect(()=> {
+    const fetchCommentsfull = async () => {
+    try {
+      const response = await fetch(`/product/${productdetail}`);
+      const data = await response.json();
+      console.log(data); // 확인용
+      setproductdata(data)
+
+  } catch (error) {
+      console.error("에러 발생 :", error);
+  }
+}})
+  console.log(productdetail)
+  return (
+    <>
+      <ProductDetail  />
+    </>
+  );
+};
+
+
--- frontend/app/components/homeComp/HomeCategoryNav/HomeCategoryNav.tsx
+++ frontend/app/components/homeComp/HomeCategoryNav/HomeCategoryNav.tsx
@@ -14,7 +14,7 @@ const HomeCategoryNav: React.FC<HomeCategoryNavProps> = ({ categories }) => {
   return (
     // 임시 스타일링: 호버시 밑줄
     <nav className='w-screen h-1/6'>
-      <ul className='flex-center gap-6'>
+      <ul className='flex justify-center items-center gap-6 h-32 font-serif font-bold text-5xl'>
         {categoryList.map((category, index) => (
           <li key={index}>
               <div className='transition-all hover:underline'>
