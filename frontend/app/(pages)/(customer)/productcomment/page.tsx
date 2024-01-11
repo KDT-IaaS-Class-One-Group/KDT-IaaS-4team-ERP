0d1d2dce988c2f1aaacac5473c00ffc67fcc7084
DongHyunShin <personarpg7501@gmail.com>
2024-01-11 12:09:33 +0900

    check : 중간저장

--- frontend/app/(pages)/(customer)/productcomment/page.tsx
+++ frontend/app/(pages)/(customer)/productcomment/page.tsx
@@ -8,51 +8,81 @@ import ProductCommentList from '@/components/ProductComment/ProductCommentList';
 import ProductUploadButton from '@/components/ProductComment/ProductUploadButton';
 import Link from 'next/link';
 
-const CommentHome = () => {
-  const [comments, setComments] = useState([]);
+interface productcommentfull {
+  userId: string;
+  reviewUpdatedAt: Date;
+  reviewRating: string;
+  reviewTitle: string;
+}
 
+
+export default function Productcommentfull() {
+  const [productwhole, setProductComment] = useState([])
   useEffect(() => {
-    const fetchComments = async () => {
+    const fetchProductcomment = async () => {
       try {
-        // http://192.168.100.83:3560/login에서 데이터를 가져오도록 수정
-        const response = await fetch('http://192.168.100.83:3560/reviews');
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
+        const response = await fetch('http://localhost:3560/productcomment');
         const data = await response.json();
-        console.log(data);
-        setComments(data); // 가져온 데이터를 상태에 저장
+        console.log(data)
       } catch (error) {
-        console.error('Error fetching comments:', error);
+        console.error('데이터를 불러오는 동안 에러 발생:', error);
       }
-    };
+    }
+  })
+  return;
+}
 
-    // 데이터 조회 함수 호출
-    fetchComments();
-  }, []); // useEffect의 두 번째 매개변수로 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 함
 
-  return (
-    <>
-      <div className='w-screen h-screen flex justify-start items-center flex-col gap-6'>
-        {comments.map((comment, index) => (
-          <ProductCommentList
-            key={index}
-            writerid={comments.userId}
-            date={comments.reviewContent}
-            starcount={comments.reviewRating}
-            commenttitle={comments.reviewTitle}
-          />
-        ))}
-      </div>
-      <Link href="/productcommentwriting">
-        <div className='mr-40 mb-20'>
-          <ProductUploadButton value='상품평 등록' />
-        </div>
-      </Link>
-    </>
-  );
-};
 
-export default CommentHome;
+
+
+
+
+// const CommentHome = () => {
+//   const [comments, setComments] = useState([]);
+
+//   useEffect(() => {
+//     const fetchComments = async () => {
+//       try {
+//         // http://192.168.100.83:3560/login에서 데이터를 가져오도록 수정
+//         const response = await fetch('http://192.168.100.83:3560/productcomment');
+
+//         if (!response.ok) {
+//           throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+
+//         const data = await response.json();
+//         console.log(data);
+//         setComments(data); // 가져온 데이터를 상태에 저장
+//       } catch (error) {
+//         console.error('Error fetching comments:', error);
+//       }
+//     };
+
+//     // 데이터 조회 함수 호출
+//     fetchComments();
+//   }, []); // useEffect의 두 번째 매개변수로 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 함
+
+//   return (
+//     <>
+//       <div className='w-screen h-screen flex justify-start items-center flex-col gap-6'>
+//         {comments.map((comment, index) => (
+//           <ProductCommentList
+//             key={index}
+//             writerid={comments.userId}
+//             date={comments.reviewContent}
+//             starcount={comments.reviewRating}
+//             commenttitle={comments.reviewTitle}
+//           />
+//         ))}
+//       </div>
+//       <Link href="/productcommentwriting">
+//         <div className='mr-40 mb-20'>
+//           <ProductUploadButton value='상품평 등록' />
+//         </div>
+//       </Link>
+//     </>
+//   );
+// };
+
+// export default CommentHome;
