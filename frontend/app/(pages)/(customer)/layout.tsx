f1ed2f1ec97fc15d9940d543132728694cb88591
kimhyun <forhn37@gmail.com>
2024-01-13 12:35:22 +0900

    feat : 로그인 상태 변경 완료 후 리팩토링할때 지금은 location.href='/'로 브라우저를 재로딩하게 하였으나 추후 다른 방법이 생기면 변경하면 좋을 듯

--- frontend/app/(pages)/(customer)/layout.tsx
+++ frontend/app/(pages)/(customer)/layout.tsx
@@ -7,11 +7,7 @@ import HeaderItem from '@/components/Header/HeaderItem';
 export default function CustomerLayout({ children }: ChildrenProps) {
   return (
     <>
-      <Header>
-        <HeaderItem href='/login'>로그인/로그아웃</HeaderItem>
-        <HeaderItem href='/orderlist'>주문조회</HeaderItem>
-        <HeaderItem href='/cart'>장바구니</HeaderItem>
-      </Header>
+      <Header />
       {children}
       <Footer></Footer>
     </>
--- frontend/app/components/Header/Header.tsx
+++ frontend/app/components/Header/Header.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import ChildrenProps from '@/interfaces/ChildrenProps';
+import HeaderItem from './HeaderItem';
 
 export default function Header({ children }: ChildrenProps) {
+  const [isToken, setIsToken] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsToken(!!token); // token이 있으면 true, 없으면 false로 설정
+  }, []);
+
   return (
     <header className='flex bg-blue-500 text-white p-4 justify-between'>
       <Link href='/'>
         <h1 className='text-xl'>form 미쳤다.</h1>
       </Link>
-      <ul className='flex ml-auto'>{children}</ul>
+      <ul className='flex ml-auto'>
+        {isToken ? (
+          <>
+            <HeaderItem href='/orderlist'>주문조회</HeaderItem>
+            <HeaderItem href='/cart'>장바구니</HeaderItem>
+            <HeaderItem href='/logout'>로그아웃</HeaderItem>
+          </>
+        ) : (
+          <HeaderItem href='/login'>로그인</HeaderItem>
+        )}
+      </ul>
     </header>
   );
 }
