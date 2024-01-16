c30fed054f4381370fe8d3356ece3cbcafadf9f4
YoonJunHyeon <qmdldos@gmail.com>
2024-01-16 21:11:25 +0900

    중간 저장

--- frontend/app/components/Payment/CartToPayment/CTPInput.tsx
+++ frontend/app/components/Payment/CartToPayment/CTPInput.tsx
@@ -1,16 +1,9 @@
+import { CTPInputProps } from "@/interfaces/CartToPayment/CTPInputProps";
 
 
-
-interface CTPInputProps {
-  InputName: string;
-  inputPlaceholder: string;
-  listName: string;
-  liClassName: string;
-}
 /**
  * CartToPayment 컴포넌트를 위한 입력 필드를 렌더링합니다.
  *
- * @param {Object} props - 컴포넌트 props입니다.
  * @param {string} props.InputName - 입력 필드의 이름입니다.
  * @param {string} props.inputPlaceholder - 입력 필드의 플레이스홀더 텍스트입니다.
  * @param {string} props.listName - 리스트의 이름입니다.
@@ -20,8 +13,8 @@ interface CTPInputProps {
 export default function CTPInput({ InputName, inputPlaceholder, listName, liClassName }: CTPInputProps) {
   return (
     <li className={liClassName}>
-      <p>{`${listName} : `}</p>
-      <input type="text" name={InputName} placeholder={inputPlaceholder} id={InputName} />
+      <p className="w-1/4 text-right">{`${listName} : `}</p>
+      <input className="w-3/4 h-full outline outline-1" type="text" name={InputName} placeholder={inputPlaceholder} id={InputName} />
     </li>
   );
 }
