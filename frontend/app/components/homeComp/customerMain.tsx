ece6134ac2dd2e869c0e1d539c438d8bfb2b5204
kimhyun <forhn37@gmail.com>
2024-01-16 15:50:16 +0900

    feat : 기존 코드 최종 점검

--- frontend/app/components/homeComp/customerMain.tsx
+++ frontend/app/components/homeComp/customerMain.tsx
@@ -26,7 +26,7 @@ export default function CustomerMain(){
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await fetch('http://localhost:3570/');  // 서버 주소에 맞게 변경해주세요
+        const response = await fetch('http://localhost:3560/');  // 서버 주소에 맞게 변경해주세요
         const data = await response.json();
         console.log(data)
         const extractedData = data.map(list => ({
