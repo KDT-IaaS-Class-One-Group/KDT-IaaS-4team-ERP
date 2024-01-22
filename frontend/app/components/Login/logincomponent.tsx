891fd0b68786eabb44aba2c8655d7b0cf471ad04
YoonJunHyeon <qmdldos@gmail.com>
2024-01-22 14:59:34 +0900

    feat : 로그인 페이지 input tpye 변경 add - loginPassword.tsx 생성 fix - loginConponent 수정

--- frontend/app/components/Login/logincomponent.tsx
+++ frontend/app/components/Login/logincomponent.tsx
@@ -1,17 +1,18 @@
-'use client';
-import React, { ChangeEvent } from 'react';
+"use client";
+import React, { ChangeEvent } from "react";
 
-import Link from 'next/link';
-import LoginButton from './loginbutton';
-import LoginText from './logintext';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import Link from "next/link";
+import LoginButton from "./loginbutton";
+import LoginText from "./logintext";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import LoginPassword from "./loginPassword";
 
 const LoginHome = () => {
   const router = useRouter();
   const [loginUser, setLoginUser] = useState({
-    userId: '',
-    userPassword: '',
+    userId: "",
+    userPassword: "",
   });
 
   const handleInputChange = (field: string, value: string) => {
@@ -26,9 +27,9 @@ const LoginHome = () => {
   const handleLogin = async () => {
     try {
       const response = await fetch(`http://localhost:3560/login`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           userId: loginUser.userId,
@@ -37,41 +38,43 @@ const LoginHome = () => {
       });
 
       if (!response.ok) {
-        throw new Error('로그인 실패');
+        throw new Error("로그인 실패");
       }
 
       const data = await response.json();
       console.log(data);
       if (data.success) {
-        location.href='/'
-        localStorage.setItem('token', data.token)
-        alert('로그인 성공');
+        location.href = "/";
+        localStorage.setItem("token", data.token);
+        alert("로그인 성공");
       } else {
-        alert('로그인 실패');
+        alert("로그인 실패");
       }
     } catch (error) {
       console.error(error);
-      alert('로그인 실패');
+      alert("로그인 실패");
     }
   };
 
   return (
-    <div className='flex justify-center items-center flex-col h-2/6 w-2/6'>
-      <div className='h-2/5 flex flex-col justify-around items-center w-full'>
+    <div className="flex justify-center items-center flex-col h-2/6 w-2/6">
+      <div className="h-2/5 flex flex-col justify-around items-center w-full">
         <LoginText
-          title='ID'
-          inputchange={(value: string) => handleInputChange('userId', value)}
+          title="ID"
+          inputchange={(value: string) => handleInputChange("userId", value)}
         />
-        <LoginText
-          title='PASSWORD'
-          inputchange={(value: string) => handleInputChange('userPassword', value)}
+        <LoginPassword
+          title="PASSWORD"
+          inputchange={(value: string) =>
+            handleInputChange("userPassword", value)
+          }
         />
       </div>
-      <div className='h-1/5 flex items-center justify-end w-full'>
-        <Link href='/signup'>
-          <LoginButton value='sign up' onClick={handleSignup} />
+      <div className="h-1/5 flex items-center justify-end w-full">
+        <Link href="/signup">
+          <LoginButton value="sign up" onClick={handleSignup} />
         </Link>
-        <LoginButton value='login' onClick={handleLogin} />
+        <LoginButton value="login" onClick={handleLogin} />
       </div>
     </div>
   );
