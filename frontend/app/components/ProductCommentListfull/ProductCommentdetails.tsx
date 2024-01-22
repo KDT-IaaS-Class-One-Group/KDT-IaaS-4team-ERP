49ff6a557fc8b30ca87938f9134b726fe6387b09
YoonJunHyeon <qmdldos@gmail.com>
2024-01-22 19:04:28 +0900

    style : ProductCommentdetails 컴포넌트 스타일 수정
    정렬 및 Btn 컴포넌트 재사용
    이제 댓글 컴포넌트 작업시작

--- frontend/app/components/ProductCommentListfull/ProductCommentdetails.tsx
+++ frontend/app/components/ProductCommentListfull/ProductCommentdetails.tsx
@@ -1,12 +1,22 @@
 // ? 제품상품평페이지 컴포넌트 작성
 import { useRouter } from "next/navigation";
-const ProductCommentdetails = ({ reviewTitle, reviewContent, reviewRating }) => {
+import Btn from "../Btn/Btn";
+
+interface ProductCommentdetailsProps {
+  reviewTitle: string;
+  reviewContent: string;
+  reviewRating: string;
+}
+
+const ProductCommentdetails = ({
+  reviewTitle,
+  reviewContent,
+  reviewRating,
+}: ProductCommentdetailsProps) => {
+  // useRouter를 활용한 url 조작 함수
   const router = useRouter();
-
-
-
   function goback() {
-    router.push('/')
+    router.push("/product");
   }
   return (
     <div className="flex flex-col items-center bg-blue-200 text-white w-full h-full justify-center">
@@ -24,17 +34,26 @@ const ProductCommentdetails = ({ reviewTitle, reviewContent, reviewRating }) => 
         </div>
       </div>
 
-      <div className="h-20 mb-4 flex justify-start w-4/5 items-center">
-        <ul className="text-2xl text-black">평점 : </ul>
-          <div className='flex items-center justify-start text-4xl'>
+      {/* Area3 : 평점 및 뒤로가기 버튼 */}
+      <div className="h-20 mt-4 flex justify-between w-4/5 items-center">
+        <ul className="flex justify-start items-center gap-3">
+          <p className="text-2xl text-slate-950">평점 :</p>
+          <div className="flex items-center justify-start text-4xl mb-2">
             {Array.from({ length: parseInt(reviewRating, 10) }).map((_, i) => (
-              <span className="text-yellow-500"key={i}>★</span>
+              <span className="text-yellow-500 block" key={i}>
+                ★
+              </span>
             ))}
           </div>
-          <button type="button" onClick={goback}>뒤로가기</button>
+        </ul>
+        <Btn
+          textContent="뒤로가기"
+          className="h-10 w-28 text-xs flex items-center bg-slate-50 text-slate-500 justify-center "
+          onClick={goback}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductCommentdetails;
