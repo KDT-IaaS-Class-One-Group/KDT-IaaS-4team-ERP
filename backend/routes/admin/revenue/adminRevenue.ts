41b9cc2283090a9c0017a1592e052e2275a13652
HongMoonGI <hmoongi94@gmail.com>
2024-01-23 15:53:49 +0900

    feat: 카테고리별로 분류해서 총 판매량 구하는 로직 추가.

--- backend/routes/admin/revenue/adminRevenue.ts
+++ backend/routes/admin/revenue/adminRevenue.ts
@@ -14,15 +14,15 @@ interface Order {
 }
 
 adminRevenue.get("/api/adminRevenue", async (req: Request, res: Response) => {
-  // 클라이언트로부터 startDate와 endDate 파라미터를 가져옴
-  const { startDate, endDate } = req.query;
+  // 클라이언트로부터 startDate, endDate, category 파라미터를 가져옴
+  const { startDate, endDate, category } = req.query;
   console.log(req.query);
 
   try {
     const conn = await pool.getConnection();
 
-    // startDate와 endDate를 이용하여 해당 기간 동안의 매출 데이터를 조회하는 쿼리 작성
-    const query = `
+    // startDate와 endDate, category를 이용하여 해당 기간 동안의 매출 데이터를 조회하는 쿼리 작성
+    let query = `
       SELECT 
         orders.orderIndex, 
         orders.orderPaymentCount, 
@@ -30,15 +30,25 @@ adminRevenue.get("/api/adminRevenue", async (req: Request, res: Response) => {
         orders.orderPaymentTotalPrice, 
         orders.userIndex, 
         orders.prodIndex, 
-        products.prodName
+        products.prodName,
+        products.prodCategory
       FROM orders 
       JOIN products ON orders.prodIndex = products.prodIndex
       WHERE orders.orderPaymentDatetime BETWEEN ? AND ?`;
 
-    const revenuedata: Order[] = (await conn.query(query, [
-      startDate,
-      endDate,
-    ])) as Order[];
+    // category가 전달된 경우에만 해당 카테고리의 상품 조회
+    if (category) {
+      query += ` AND products.prodCategory = ?`;
+    }
+
+    const queryParams = [startDate, endDate];
+    
+    // category가 전달된 경우에만 쿼리 파라미터에 추가
+    if (category) {
+      queryParams.push(category);
+    }
+
+    const revenuedata: Order[] = (await conn.query(query, queryParams)) as Order[];
     console.log(revenuedata);
 
     // 총 판매량 계산
