4a65d4cf00b28c7643c5d3eb765c0980e3a0c726
HongMoonGI <hmoongi94@gmail.com>
2024-01-23 19:48:11 +0900

    feat: css작업 중.. 테일윈드 미숙 + 디자인감각미숙으로 작업이 힘듬

--- frontend/app/components/Payment/deliveryInformation.tsx
+++ frontend/app/components/Payment/deliveryInformation.tsx
@@ -21,31 +21,31 @@ const DeliveryInformation: React.FC<Productpaymentprops> = ({
   // const buyerinformation = await fetchbuyerinformationdata()
 
   return (
-    <div className="flex justify-center items-center mt-5">
+    <div className="w-screen h-1/4 border border-slate-950 flex justify-around items-center">
       <div
-        className={`${styles.buyerInformation} flex flex-col justify-center h-3/5`}
+        className={`flex flex-col justify-center h-3/5`}
       >
         {/* tailwind와 css모듈 같이 사용하기 */}
         <div className="flex w-full justify-around mt-2 mb-2 ">
           <label htmlFor="deliveryname">이름:</label>
-          <input type ='text' className='w-3/5' id ='deliveryname'  onChange={handleInputChange("orderReceiver")} />
+          <input type ='text' className='w-5/6' id ='deliveryname'  onChange={handleInputChange("orderReceiver")} />
           {/* {deliveryinfo.productData} */}
         </div>
 
         <div className="flex w-full justify-around mt-2 mb-2 ">
         <label htmlFor="deliveryphone">연락처:</label>
-          <input type ='text' className='w-3/5' id ='deliveryphone' onChange={handleInputChange("orderReceiverPhone")} />
+          <input type ='text' className='w-5/6' id ='deliveryphone' onChange={handleInputChange("orderReceiverPhone")} />
 
         </div>
 
         <div className="flex w-full justify-around mt-2 mb-2 ">
         <label htmlFor="deliveryaddress">주소:</label>
-          <input  type ='text' className='w-3/5' id ='deliveryaddress' onChange={handleInputChange("orderDeliveryAddress")} />
+          <input  type ='text' className='w-5/6' id ='deliveryaddress' onChange={handleInputChange("orderDeliveryAddress")} />
         </div>
 
         <div className="flex w-full justify-around mt-2 mb-2 ">
         <label htmlFor="deliveryrequest">요청사항:</label>
-          <input type ='text' className='w-3/5 ' id ='deliveryrequest' onChange={handleInputChange("orderRequest")} />
+          <input type ='text' className='w-5/6 ' id ='deliveryrequest' onChange={handleInputChange("orderRequest")} />
 
         </div>
       </div>
