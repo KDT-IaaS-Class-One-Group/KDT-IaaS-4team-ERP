6402e5c1c93346f39ac9de0c2cd766dfdc29d937
이민구 <escapeweedy@gmail.com>
2024-01-24 16:44:44 +0900

    feat: 불필요 파일 삭제 (주문 수락, 주문 거절)

--- frontend/app/(pages)/admin/(dashboard)/page.tsx
+++ frontend/app/(pages)/admin/(dashboard)/page.tsx
@@ -5,14 +5,5 @@ import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function AdminPage() {
-  const { isLoggedIn } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isLoggedIn) {
-      alert('로그인을 해주세요');
-      router.push('/admin/login');
-    }
-  }, [isLoggedIn, router]);
   return <div>test</div>;
 }
