e611802dadea94b64b41399c969322e9e0cc9a05
DongHyunShin <personarpg7501@gmail.com>
2024-01-25 16:53:58 +0900

    feat : Modal 숨기는 로직은 hidden에서 visibility:hidden에서 display:none 으로 수정

--- frontend/app/components/Modal/Modal.tsx
+++ frontend/app/components/Modal/Modal.tsx
@@ -8,34 +8,32 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, message }) => {
-    // 모달이 열려 있을 때 body에 'overflow-hidden' 클래스 추가
-    if (isOpen) {
-        document.body.classList.add('overflow-hidden');
-    } else {
-        document.body.classList.remove('overflow-hidden');
-    }
-
     return (
         <>
             {isOpen && (
-                <div className="modal-overlay fixed inset-0 bg-black opacity-50"></div>
+                <>
+                    <div className="modal-overlay fixed inset-0 bg-black opacity-50"></div>
+                    <div
+                        className="modal fixed inset-0 z-50 flex items-center justify-center"
+                        style={{ display: isOpen ? 'flex' : 'none' }}
+                    >
+                        <div className="modal bg-white p-4 rounded shadow-lg">
+                            <div className="modal-header flex justify-between items-center border-b pb-2 mb-2">
+                                <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
+                                <button
+                                    onClick={onClose}
+                                    className="text-sm text-gray-500 hover:text-gray-700 focus:outline-none"
+                                >
+                                    확인
+                                </button>
+                            </div>
+                            <div className="modal-body p-6">
+                                <p className="text-gray-800">{message}</p>
+                            </div>
+                        </div>
+                    </div>
+                </>
             )}
-            <div className={`fixed inset-0 z-50 flex items-center justify-center ${isOpen ? '' : 'hidden'}`}>
-                <div className="modal bg-white p-4 rounded shadow-lg">
-                    <div className="modal-header flex justify-between items-center border-b pb-2 mb-2">
-                        <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
-                        <button
-                            onClick={onClose}
-                            className="text-sm text-gray-500 hover:text-gray-700 focus:outline-none"
-                        >
-                            확인
-                        </button>
-                    </div>
-                    <div className="modal-body p-6">
-                        <p className="text-gray-800">{message}</p>
-                    </div>
-                </div>
-            </div>
         </>
     );
 };
