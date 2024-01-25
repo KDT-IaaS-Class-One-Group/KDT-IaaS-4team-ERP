fc87be29ede1de8ba5fe570012b0ed9c990688ff
YoonJunHyeon <qmdldos@gmail.com>
2024-01-25 16:26:59 +0900

    style : order-manage cursor-pointer 추가

--- frontend/app/(pages)/admin/(dashboard)/(sale)/order-manage/page.tsx
+++ frontend/app/(pages)/admin/(dashboard)/(sale)/order-manage/page.tsx
@@ -1,19 +1,19 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-import { orderIndexProp } from '@/app/types/Order/OrderIndexProp';
-import { OrderRequestProp } from '@/app/types/Order/OrderRequestProp';
-import { OrderDeliveryAddressProp } from '@/app/types/Order/OrderDeliveryAddressProp';
-import { OrderPaymentCountProp } from '@/app/types/Order/OrderPaymentCountProp';
-import { OrderPaymentDatetimeProps } from '@/app/types/Order/OrderPaymentDatetimeProp';
-import { OrderPaymentPriceAtOrderProp } from '@/app/types/Order/OrderPaymentPriceAtOrderProp';
-import { OrderDeliveryDoneProp } from '@/app/types/Order/OrderDeliveryDoneProp';
-import { OrderReceiverPhoneProp } from '@/app/types/Order/OrderReceiverPhoneProp';
-import { UserIndexProp } from '@/app/types/Order/UserIndexProp';
+"use client";
+import React, { useState, useEffect } from "react";
+import { orderIndexProp } from "@/app/types/Order/OrderIndexProp";
+import { OrderRequestProp } from "@/app/types/Order/OrderRequestProp";
+import { OrderDeliveryAddressProp } from "@/app/types/Order/OrderDeliveryAddressProp";
+import { OrderPaymentCountProp } from "@/app/types/Order/OrderPaymentCountProp";
+import { OrderPaymentDatetimeProps } from "@/app/types/Order/OrderPaymentDatetimeProp";
+import { OrderPaymentPriceAtOrderProp } from "@/app/types/Order/OrderPaymentPriceAtOrderProp";
+import { OrderDeliveryDoneProp } from "@/app/types/Order/OrderDeliveryDoneProp";
+import { OrderReceiverPhoneProp } from "@/app/types/Order/OrderReceiverPhoneProp";
+import { UserIndexProp } from "@/app/types/Order/UserIndexProp";
 
-import { ProductsTableProps } from '@/app/types/Product/ProductsTableProps';
-import Image from 'next/image';
-import { formatDate } from '@/app/utils/formatDate';
-import { OrderReceiverProp } from '@/app/types/Order/OrderReceiverProp';
+import { ProductsTableProps } from "@/app/types/Product/ProductsTableProps";
+import Image from "next/image";
+import { formatDate } from "@/app/utils/formatDate";
+import { OrderReceiverProp } from "@/app/types/Order/OrderReceiverProp";
 
 interface OrderProps
   extends orderIndexProp,
@@ -36,11 +36,11 @@ export default function OrderManagement() {
     // 주문 데이터 불러오기
     const fetchOrders = async () => {
       try {
-        const response = await fetch('http://localhost:3560/api/orders');
+        const response = await fetch("http://localhost:3560/api/orders");
         const data = await response.json();
         setOrders(data);
       } catch (error) {
-        console.error('주문 데이터 로딩 오류:', error);
+        console.error("주문 데이터 로딩 오류:", error);
       }
     };
     fetchOrders();
@@ -50,18 +50,18 @@ export default function OrderManagement() {
   // 주문 수락 또는 거절 처리 함수
   const handleOrderAcceptance = async (
     orderIndex: number,
-    isAccepted: boolean,
+    isAccepted: boolean
   ) => {
     try {
       const response = await fetch(
         `http://localhost:3560/api/orders/acceptance/${orderIndex}`,
         {
-          method: 'PATCH',
+          method: "PATCH",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({ isAccepted }),
-        },
+        }
       );
 
       const data = await response.json();
@@ -73,13 +73,13 @@ export default function OrderManagement() {
               return { ...order, orderIsOrderAccepted: isAccepted ? 1 : 2 };
             }
             return order;
-          }),
+          })
         );
       } else {
         // 오류 처리
       }
     } catch (error) {
-      console.error('주문 상태 업데이트 오류:', error);
+      console.error("주문 상태 업데이트 오류:", error);
     }
   };
 
@@ -89,12 +89,12 @@ export default function OrderManagement() {
       const response = await fetch(
         `http://localhost:3560/api/orders/delivery/${orderIndex}`,
         {
-          method: 'PATCH',
+          method: "PATCH",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify({ deliveryStatus: status }),
-        },
+        }
       );
 
       const data = await response.json();
@@ -106,58 +106,58 @@ export default function OrderManagement() {
               return { ...order, orderDeliveryDone: status };
             }
             return order;
-          }),
+          })
         );
       } else {
         // 오류 처리
-        console.error('배송 상태 업데이트 실패');
+        console.error("배송 상태 업데이트 실패");
       }
     } catch (error) {
-      console.error('배송 상태 업데이트 오류:', error);
+      console.error("배송 상태 업데이트 오류:", error);
     }
   };
 
   return (
-    <div className='flex container mx-auto p-4 items-center flex-col'>
-      <div className='overflow-x-auto w-full'>
-        <div className='max-h-[800px] overflow-y-auto'>
-          <table className='min-w-full leading-normal'>
-            <thead className='sticky top-0 bg-gray-500'>
+    <div className="flex container mx-auto p-4 items-center flex-col">
+      <div className="overflow-x-auto w-full">
+        <div className="max-h-[800px] overflow-y-auto">
+          <table className="min-w-full leading-normal">
+            <thead className="sticky top-0 bg-gray-500">
               <tr>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   주문 번호
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   이미지
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   상품명
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   요청 사항
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   수령인
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   배송 주소
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   연락처
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   주문 개수
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   주문 날짜
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   주문 금액
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   배송 상태
                 </th>
-                <th className='px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider'>
+                <th className="px-5 py-3 border-b-2 border-wine text-yellow-300 text-left text-xs font-semibold uppercase tracking-wider">
                   조치
                 </th>
               </tr>
@@ -165,71 +165,71 @@ export default function OrderManagement() {
             <tbody>
               {orders.map((order) => (
                 <tr key={order.orderIndex}>
-                  <td className='px-5 py-3 border-b text-sm'>
-                    <div className='whitespace-no-wrap'>{order.orderIndex}</div>
+                  <td className="px-5 py-3 border-b text-sm">
+                    <div className="whitespace-no-wrap">{order.orderIndex}</div>
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
+                  <td className="px-5 py-3 border-b text-sm">
                     <Image
                       width={50}
                       height={50}
-                      className='w-full h-full rounded-full'
+                      className="w-full h-full rounded-full"
                       src={`/images${order.prodImgUrl}`}
-                      alt='image'
+                      alt="image"
                     />
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
+                  <td className="px-5 py-3 border-b text-sm">
                     {order.prodName}
                   </td>
 
-                  <td className='px-5 py-3 border-b text-sm'>
-                    <div className='whitespace-no-wrap'>
+                  <td className="px-5 py-3 border-b text-sm">
+                    <div className="whitespace-no-wrap">
                       {order.orderRequest}
                     </div>
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
-                    <div className='whitespace-no-wrap'>
+                  <td className="px-5 py-3 border-b text-sm">
+                    <div className="whitespace-no-wrap">
                       {order.orderReceiver}
                     </div>
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
-                    <div className='whitespace-no-wrap'>
+                  <td className="px-5 py-3 border-b text-sm">
+                    <div className="whitespace-no-wrap">
                       {order.orderDeliveryAddress}
                     </div>
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
-                    <div className='whitespace-no-wrap'>
+                  <td className="px-5 py-3 border-b text-sm">
+                    <div className="whitespace-no-wrap">
                       {order.orderReceiverPhone}
                     </div>
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
-                    <div className='whitespace-no-wrap'>
+                  <td className="px-5 py-3 border-b text-sm">
+                    <div className="whitespace-no-wrap">
                       {order.orderPaymentCount}
                     </div>
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
-                    <div className='whitespace-no-wrap'>
+                  <td className="px-5 py-3 border-b text-sm">
+                    <div className="whitespace-no-wrap">
                       {formatDate(order.orderPaymentDatetime)}
                     </div>
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
-                    <div className='whitespace-no-wrap'>
+                  <td className="px-5 py-3 border-b text-sm">
+                    <div className="whitespace-no-wrap">
                       {order.orderPaymentPriceAtOrder}원
                     </div>
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
-                    <div className='whitespace-no-wrap'>
+                  <td className="px-5 py-3 border-b text-sm">
+                    <div className="whitespace-no-wrap">
                       {order.orderDeliveryDone === 0
-                        ? '배송 준비'
-                        : '배송 완료'}
+                        ? "배송 준비"
+                        : "배송 완료"}
                     </div>
                   </td>
-                  <td className='px-5 py-3 border-b text-sm'>
+                  <td className="px-5 py-3 border-b text-sm">
                     {order.orderDeliveryDone === 0 && (
                       <button
                         onClick={() =>
                           handleDeliveryStatus(order.orderIndex, 1)
                         }
-                        className='text-blue-500 hover:text-blue-800 ml-4'
+                        className="text-blue-500 hover:text-blue-800 ml-4 cursor-pointer"
                       >
                         배송하기
                       </button>
